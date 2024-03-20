183aa873ec93bd4ccb2616ab582b1ba0b8ec4dd1
MinzPark <114046169+MinzPark@users.noreply.github.com>
2024-03-20 15:11:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const googleApiKey = 'AIzaSyCILG5BjAauojzUS_dEUNELFfGobKlw2bE';
-export const googleSheetDocId = '1oY2He8z3pul_TPXizblo65QMMCU0PqSAKobN5KxVOXo';
+export const googleSheetDocId = '144ARuIqFshO3qgX7pDUsrX_rURTYY-xt7A3y30vv9qoo';
